01929dfef44a105c97dfd6f3aa4bc7bfd74304ec
unknown <arthurferreira1417@gmail.com>
2024-04-30 17:15:56 -0300

    New: aprendendo a usar exponenciação em calculos

---
+++ op_expresssoes/op_modulo/index.php
@@ -0,0 +1,17 @@
+<?php
+echo 12 % 2;
+echo "<br/> moduo <br/>";
+echo 12 / 2;
+echo "<br/> divisao <br/>";
+echo 13 % 2;
+echo "<br/> moduo <br/>";
+echo 12 % 5;
+echo "<br/> modiulo <br/>";
+
+$a = 12;
+$b = 4;
+$c = 13;
+echo $a % $b;
+echo "<br/>";
+echo $a % $c;
+?>
