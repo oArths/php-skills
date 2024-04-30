111f0941f05a32accbdd0bc23da043583b794602
unknown <arthurferreira1417@gmail.com>
2024-04-30 17:15:16 -0300

    New: aprendendo a ordem dos cauculos e suas prioridades

---
+++ op_expresssoes/operadores/index.php
@@ -0,0 +1,15 @@
+<?php
+$soma = 12 + 12;
+echo $soma . "<br/>";
+
+$divisao = 12 - 22;
+echo $divisao . "<br/>";
+
+$multi = 12 * 12;
+echo $multi . "<br/>";
+
+$divi = 12 / 12;
+echo $divi . "<br/>";
+
+
+?>
---
+++ op_expresssoes/ordem_op/index.php
@@ -0,0 +1,20 @@
+<?php
+echo 3 + 3 * 5;
+echo "<br/>";
+echo (3 + 3) * 5;
+echo "<br/>";
+echo 3 + 3 / 5;
+echo "<br/>";
+
+$a = 3;
+$b = 4;
+$c = 5;
+echo $a + $b / $c;
+echo "<br/>";
+echo $b + $c / $a;
+echo "<br/>";
+
+$d = $b + $c / $a;
+echo $d;
+
+?>
