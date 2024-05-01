32e3de0d50e7597f25279183f3461f3c0d905e18
unknown <arthurferreira1417@gmail.com>
2024-04-30 22:09:33 -0300

    New: aprendendo a usar o cast

---
+++ op_expresssoes/cast/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = (int) "12";
+echo $a . "<br/>";
+echo $a + 10 . "<br/>";
+echo gettype($a)  ;
+echo"<br/>";
+
+$a = (float) "12.2323";
+echo $a . "<br/>";
+echo $a + 10 . "<br/>";
+echo gettype($a)  ;
+
+?>
