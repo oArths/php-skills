564774783c9e897e3a0e34baa067a5dbe190920b
unknown <arthurferreira1417@gmail.com>
2024-04-30 22:08:29 -0300

    New: aprendendo a usar contetanação de elemntos

---
+++ op_expresssoes/concatenacao/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "testando a concatenação";
+echo "<br/>";
+echo "testando " . "a" . " contatenação";
+echo "<br/>";
+$t = "testando";
+$c = "concatenação";
+echo $t . " a " . $c; 
+
+?>
